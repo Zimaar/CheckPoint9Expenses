1481ee2dfeaa0450ec0ca9b43db9522df7b879a3
Luming-Li <75204691+Luming-Li@users.noreply.github.com>
2020-12-06 17:14:44 +0800

    ExpenseSystem.java
    Complete analysis mode
    add array list expense list in main for calculation
    add expense list append in edit mode choice 1
    add edit mode choice 3 for quit

--- src/ExpenseSystem.java
+++ src/ExpenseSystem.java
@@ -1,7 +1,44 @@
 import java.util.*;
 
+class Expense {
+
+    private String description;
+    private String category;
+    private int amount;
+
+    public Expense(String description, String category, int amount) {
+        this.description = description;
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setDescription(String newDescription) {
+        this.description = newDescription;
+    }
+    public void setCategory(String newCategory) {
+        this.category = newCategory;
+    }
+    public void setAmount(int newAmount) {
+        this.amount = newAmount;
+    }
+
+    public String toString() {
+        return ("Description: " + description + " Catagory:" + category + " Amount: " + amount);
+    }
+}
 public class ExpenseSystem {
     static Scanner scan = new Scanner(System.in);
+    static List<Expense> expenselist = new ArrayList<>();
 
     public static void main(String[] args) {
         int choice = 0;
@@ -18,6 +55,7 @@ public class ExpenseSystem {
 
             if (choice == 1) {
                 editMode();
+                System.out.println(expenselist);
             }
 
             else if (choice == 2) {
@@ -36,12 +74,13 @@ public class ExpenseSystem {
 
     }
 
-    public void editMode() {
+    public static void editMode() {
         int choice = 0;
         while (true) {
             System.out.println("====================");
             System.out.println("1) Add an Expense");
             System.out.println("2) Delete an Expense");
+            System.out.println("3) Quit");
             System.out.println("====================");
 
             choice = scan.nextInt();
@@ -54,15 +93,64 @@ public class ExpenseSystem {
                 String category = scan.next();
                 System.out.print("Enter the expense amount: ");
                 int amount = scan.nextInt();
-
-                Expense.setDescription(description);
-                Expense.setCategory(category);
-                Expense.setAmount(amount);
+                Expense free = new Expense(description,category,amount);
+                expenselist.add(free);
+                //Expense.setDescription(description);
+                //Expense.setCategory(category);
+                //Expense.setAmount(amount);
+            }
+            else if (choice == 3){
+                break;
             }
         }
     }
 
     public static void analysisMode() {
-
+        ArrayList<ArrayList<Expense>> newlist = new ArrayList<ArrayList<Expense>>();
+        ArrayList<String> categoryList = new ArrayList<>();
+        for (int i = 0; i<expenselist.size(); i++){
+            Expense var = expenselist.get(i);
+            if (categoryList.contains(var.getCategory())){
+                int j = categoryList.indexOf(var.getCategory());
+                (newlist.get(j)).add(var);
+            }
+            else{
+                ArrayList<Expense> tempList = new ArrayList<>();
+                tempList.add(var);
+                newlist.add(tempList);
+                categoryList.add(var.getCategory());
+            }
+        }
+        for (int i = 0; i<categoryList.size(); i++){
+            System.out.println("Category "+categoryList.get(i));
+            for (int k = 0; k<newlist.size(); k++){
+                System.out.println(newlist.get(k));
+            }
+        }
+        int totalsum = 0;
+        for (int k = 0; k<newlist.size(); k++){
+            int sum = 0;
+            float average = 0;
+            for (int c = 0;c<newlist.get(k).size(); c++){
+                sum = sum+newlist.get(k).get(c).getAmount();
+            }
+            average = (float)sum/newlist.get(k).size();
+            System.out.println("Sum expense of category "+categoryList.get(k)+" is: "+sum);
+            System.out.println("Average expense of category "+categoryList.get(k)+" is: "+average);
+            totalsum = totalsum+sum;
+        }
+        System.out.println("Total sum of all expense is: "+totalsum);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
