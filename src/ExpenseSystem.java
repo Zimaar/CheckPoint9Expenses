b3591744944b89ecd9def69bebfe9eae987613b0
Luming-Li <75204691+Luming-Li@users.noreply.github.com>
2020-12-08 16:42:11 +0800

    Update ExpenseSystem.java
    Update average calculation.

--- src/ExpenseSystem.java
+++ src/ExpenseSystem.java
@@ -20,7 +20,6 @@ class ExpenseSystem {
 
             if (choice == 1) {
                 editMode();
-                System.out.println(expenseList);
             }
 
             else if (choice == 2) {
@@ -73,40 +72,44 @@ class ExpenseSystem {
             }
         }
     }
-
     public static void analysisMode() {
-        ArrayList<ArrayList<Expense>> newList = new ArrayList<ArrayList<Expense>>();
+        ArrayList<ArrayList<Expense>> newlist = new ArrayList<ArrayList<Expense>>();
         ArrayList<String> categoryList = new ArrayList<>();
-        for (Expense var : expenseList) {
-            if (categoryList.contains(var.getCategory())) {
+        for (int i = 0; i< expenseList.size(); i++){
+            Expense var = expenseList.get(i);
+            if (categoryList.contains(var.getCategory())){
                 int j = categoryList.indexOf(var.getCategory());
-                (newList.get(j)).add(var);
-            } else {
+                (newlist.get(j)).add(var);
+            }
+            else{
                 ArrayList<Expense> tempList = new ArrayList<>();
                 tempList.add(var);
-                newList.add(tempList);
+                newlist.add(tempList);
                 categoryList.add(var.getCategory());
             }
         }
-        for (String s : categoryList) {
-            System.out.println("Category " + s);
-            for (ArrayList<Expense> expenses : newList) {
-                System.out.println(expenses);
+        for (int i = 0; i<categoryList.size(); i++){
+            System.out.println("Category "+categoryList.get(i));
+            for (int k = 0; k<newlist.size(); k++){
+                System.out.println(newlist.get(k));
             }
         }
         int totalSum = 0;
-        for (int k = 0; k<newList.size(); k++){
+        int count = 0;
+        for (int k = 0; k<newlist.size(); k++){
             int sum = 0;
-            float average;
-            for (int c = 0;c<newList.get(k).size(); c++){
-                sum = sum+newList.get(k).get(c).getAmount();
+            float average = 0;
+            for (int c = 0;c<newlist.get(k).size(); c++){
+                sum = sum+newlist.get(k).get(c).getAmount();
             }
-            average = (float)sum/newList.get(k).size();
+            average = (float)sum/newlist.get(k).size();
             System.out.println("Sum expense on " + categoryList.get(k)+" is: "+sum);
             System.out.println("Average expense on "+categoryList.get(k)+" is: "+average);
             totalSum = totalSum+sum;
+            count = count+newlist.get(k).size();
         }
         System.out.println("Total sum of all expense is: "+totalSum);
+        System.out.println("Average of all expense is: "+((float)totalSum/count));
     }
 }
 
